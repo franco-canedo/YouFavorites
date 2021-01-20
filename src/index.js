a00fdaa6906e3db10c6e737f5b31116ff7e9f7bc
franco-canedo <francanedo1@hotmail.com>
2021-01-19 18:14:05 -0600

    variables in .env

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import allReducers from './reducers';
 
 
+
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
