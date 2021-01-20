d151dfb8b861a1922ee6d67d271be806bcbd9621
franco-canedo <francanedo1@hotmail.com>
2021-01-20 00:39:49 -0600

    api root changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
-export const API_ROOT = 'http://localhost:3000';
+// export const API_ROOT = 'http://localhost:3000';
+export const API_ROOT = 'https://youfavsbackend.herokuapp.com';
 
 export const HEADERS = {
     'Content-Type': 'application/json',
